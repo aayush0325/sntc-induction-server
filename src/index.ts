b840d39663b01bb9fd03d4d65338df16c00542f2
aayush0325 <ayush25khanna@gmail.com>
2024-07-25 21:13:02 +0530

    setting up

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import { Hono } from "hono"
+import { Hono } from "hono";
 
-// This ensures c.env.DB is correctly typed
 type Bindings = {
   DB: D1Database;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/", async (c) => {
-  return c.json({ message: "Hello, World!" });
-});
+
+export default app;
