d5f89a188a52e3a95a19d74d03f2440de55ac3f1
aayush0325 <ayush25khanna@gmail.com>
2024-07-27 01:35:14 +0530

    db schema defined

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,12 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"; // Correct import for boolean
 
 export const users = sqliteTable("users", {
     userId: text("userId").notNull().primaryKey(),
-    clubs: text("clubs").notNull(),
+    cops: integer('COPS', { mode: 'boolean' }).default(false),
+    theQuantClub: integer('theQuantClub', { mode: 'boolean' }).default(false),
+    bizClub: integer('bizClub', { mode: 'boolean' }).default(false),
+    sae: integer('SAE', { mode: 'boolean' }).default(false),
+    amc: integer('AMC', { mode: 'boolean' }).default(false),
+    astro: integer('astroClub', { mode: 'boolean' }).default(false),
+    csi: integer('csi', { mode: 'boolean' }).default(false)
 });
