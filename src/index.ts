fd07b9740e259bed4d633bed3e55575473fc4d2a
aayush0325 <ayush25khanna@gmail.com>
2024-07-31 00:43:55 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono"
 import { clerkMiddleware } from "@hono/clerk-auth";
+import { cors } from "hono/cors";
 import userRouter from "./routes/user";
 import clubRouter from "./routes/clubs";
 
@@ -8,6 +9,8 @@ type Bindings = {
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+app.use('/api/v1/*',cors())
 
 app.use(clerkMiddleware());
 app.route('/api/v1/users',userRouter);
